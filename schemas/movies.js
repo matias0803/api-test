3b8a78c3dec755fd3c3df0883d4703fc9d2e3dac
matias garcia <mgarcias@usm.cl>
2025-05-21 19:53:42 -0400

    primer coommit api

---
+++ schemas/movies.js
@@ -0,0 +1,27 @@
+const z = require("zod");
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: "Movie title must be a string",
+    required_error: "Movie title is required",
+  }),
+  year: z.number().int().positive().min(1900).max(2026),
+  director: z.string(),
+  rate: z.number().min(0).max(10).default(0),
+  poster: z.string().url({
+    message: "poster must be a valid url",
+  }),
+  genre: z.array(z.string()),
+});
+
+function validateMovie(object) {
+  return movieSchema.safeParse(object);
+}
+function validatePartialMovie(object) {
+  return movieSchema.partial().safeParse(object);
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie,
+};
